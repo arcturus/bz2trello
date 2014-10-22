b1b58cd292190d11560cfd48c523dc098b2a2e42
Francisco Jordano <fjordano@mozilla.com>
2014-10-22 15:48:05 +0100

    Adding more information to the example to track the results

--- examples/sprint_2_7.js
+++ examples/sprint_2_7.js
@@ -1,3 +1,10 @@
+// The result of this script (with the missing data) is here:
+// https://trello.com/b/4QCltJ7s/2-1s7-automated
+//
+// Our sprint planning and bugs in bugzilla for this sprint can be
+// followed here:
+// https://wiki.mozilla.org/Gaia/Contacts/Scrum/2.1S7
+
 var boardId = '5447bb303fc7ce5ba115fef1';
 var token = '<your_token_goes_here>';
 var key = '<your_key_goes_here>';
